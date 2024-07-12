1d9d27fc23de4ff9f661b3b1dc1268a8a300ef69
Byron Wade <bcw1995@gmail.com>
2024-07-11 23:35:23 -0700

    test

--- apps/web/app/layout.jsx
+++ apps/web/app/layout.jsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<head>
-				<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 			</head>
 			<body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
