dc58d0a168f4ea5660976bc65d7bb8deb8db8a3b
Byron Wade <bcw1995@gmail.com>
2024-07-14 20:31:56 -0700

    development

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+// app/layout.js
 "use client";
 import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
@@ -8,6 +9,7 @@ import { GameProvider } from "@/context/GameContext";
 import { UserProvider } from "@/context/UserContext";
 import { KeyboardProvider } from "@/context/KeyboardContext";
 import { cn } from "@/lib/utils";
+import { useGoogleAnalytics } from "@/lib/useGoogleAnalytics";
 
 const fontSans = FontSans({
 	subsets: ["latin"],
@@ -15,10 +17,26 @@ const fontSans = FontSans({
 });
 
 export default function RootLayout({ children }) {
+	useGoogleAnalytics();
+
 	return (
 		<html lang="en">
 			<head>
 				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+				{/* Global Site Tag (gtag.js) - Google Analytics */}
+				<script async src={`https://www.googletagmanager.com/gtag/js?id=G-FX184YC75H`}></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FX184YC75H', {
+                page_path: window.location.pathname,
+              });
+            `,
+					}}
+				/>
 			</head>
 			<body className={cn("min-h-screen bg-white dark:bg-black font-sans antialiased", fontSans.variable)} suppressHydrationWarning={true}>
 				<UserProvider>
