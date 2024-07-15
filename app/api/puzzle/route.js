ffe39976ac6c8402116111f56254efd18043c82e
Byron Wade <bcw1995@gmail.com>
2024-07-14 22:27:42 -0700

    development

--- app/api/puzzle/route.js
+++ app/api/puzzle/route.js
@@ -8,8 +8,6 @@ export async function GET(request) {
 	const now = new Date();
 	const localToday = new Date(now.getTime() - now.getTimezoneOffset() * 60000).toISOString().split("T")[0];
 
-	const { data: todayData, error: todayError } = await supabase.from("puzzles").select("*").eq("puzzle_date", localToday).limit(1);
-
 	const headers = {
 		"Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
 		Pragma: "no-cache",
@@ -17,23 +15,24 @@ export async function GET(request) {
 		"Surrogate-Control": "no-store",
 	};
 
-	if (todayError) {
-		console.error("Error fetching today's puzzle:", todayError);
-		return new Response(JSON.stringify({ error: todayError.message }), { status: 500, headers });
-	}
+	try {
+		const { data: todayData, error: todayError } = await supabase.from("puzzles").select("*").eq("puzzle_date", localToday).limit(1);
 
-	if (todayData.length === 0) {
-		const { data: recentData, error: recentError } = await supabase.from("puzzles").select("*").order("puzzle_date", { ascending: false }).limit(1);
+		if (todayError) throw todayError;
 
-		if (recentError) {
-			console.error("Error fetching recent puzzle:", recentError);
-			return new Response(JSON.stringify({ error: recentError.message }), { status: 500, headers });
+		if (todayData.length === 0) {
+			const { data: recentData, error: recentError } = await supabase.from("puzzles").select("*").order("puzzle_date", { ascending: false }).limit(1);
+
+			if (recentError) throw recentError;
+
+			return new Response(JSON.stringify(recentData[0]), { status: 200, headers });
 		}
 
-		return new Response(JSON.stringify(recentData[0]), { status: 200, headers });
+		return new Response(JSON.stringify(todayData[0]), { status: 200, headers });
+	} catch (error) {
+		console.error("Error fetching puzzle data:", error);
+		return new Response(JSON.stringify({ error: error.message }), { status: 500, headers });
 	}
-
-	return new Response(JSON.stringify(todayData[0]), { status: 200, headers });
 }
 
 // Clear cache periodically
