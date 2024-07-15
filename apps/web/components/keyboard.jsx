fd97eb303c459e63008e74d6e2317e99ad2570c7
Byron Wade <bcw1995@gmail.com>
2024-07-14 18:06:52 -0700

    test

--- apps/web/components/keyboard.jsx
+++ apps/web/components/keyboard.jsx
@@ -8,13 +8,13 @@ const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
 const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
 const Key = ({ keyValue, onClick }) => (
-	<Button className="key flex justify-center items-center m-1 p-1 sm:p-2 md:p-3 rounded-md h-12 w-12 sm:h-14 sm:w-14 md:h-16 md:w-16 text-md sm:text-lg md:text-xl uppercase font-bold" onClick={() => onClick(keyValue)}>
+	<Button className="key flex justify-center items-center m-[0.10em] p-1 sm:p-2 md:p-3 rounded-md h-12 w-12 sm:h-14 sm:w-14 md:h-16 md:w-16 text-md sm:text-lg md:text-xl uppercase font-bold" onClick={() => onClick(keyValue)}>
 		{keyValue}
 	</Button>
 );
 
 const SpecialKey = ({ keyValue, onClick, variant, extraClass, children }) => (
-	<Button variant={variant} className={`key ${extraClass} flex justify-center items-center m-1 p-1 sm:p-2 md:p-3 rounded-md h-12 w-20 sm:h-14 sm:w-24 md:h-16 md:w-28 uppercase font-bold`} onClick={() => onClick(keyValue)}>
+	<Button variant={variant} className={`key ${extraClass} flex justify-center items-center m-[0.10em] p-1 sm:p-2 md:p-3 rounded-md h-12 w-20 sm:h-14 sm:w-24 md:h-16 md:w-28 uppercase font-bold`} onClick={() => onClick(keyValue)}>
 		{children || keyValue}
 	</Button>
 );
@@ -23,20 +23,20 @@ const Keyboard = () => {
 	const { handleKeyPress } = useKeyboard();
 
 	return (
-		<div className="keyboard flex flex-col items-center mt-4 w-full max-w-xl mx-auto px-2 sm:px-4 md:px-6">
+		<div className="keyboard flex flex-col items-center mt-4 w-full max-w-xl mx-auto sm:px-4 md:px-6">
 			<div className="keyboard-row flex justify-center w-full">
 				{keys1.map((key) => (
 					<Key key={key} keyValue={key} onClick={handleKeyPress} />
 				))}
 			</div>
-			<div className="keyboard-row flex justify-center w-full mt-1">
+			<div className="keyboard-row flex justify-center w-full mt-[0.10em]">
 				<div className="flex-0.5" />
 				{keys2.map((key) => (
 					<Key key={key} keyValue={key} onClick={handleKeyPress} />
 				))}
 				<div className="flex-0.5" />
 			</div>
-			<div className="keyboard-row flex justify-center w-full mt-1">
+			<div className="keyboard-row flex justify-center w-full mt-[0.10em]">
 				<SpecialKey keyValue="Enter" onClick={handleKeyPress} variant="brand" extraClass="text-xs flex-1.5">
 					Enter
 				</SpecialKey>
