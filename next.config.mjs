362703f0e21ff4e40f716716e9ab2d8785427fe8
Byron Wade <bcw1995@gmail.com>
2024-07-22 19:11:04 -0700

    added new Next.js 15rc and react 19rc for testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,27 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	swcMinify: true,
+	experimental: {
+		ppr: "incremental",
+		turbo: {
+			resolveAlias: {
+				"@components": path.resolve(__dirname, "src/components"),
+				"@utils": path.resolve(__dirname, "src/lib/utils"),
+				"@lib": path.resolve(__dirname, "src/lib"),
+				"@styles": path.resolve(__dirname, "src/styles"),
+				"@context": path.resolve(__dirname, "src/context"),
+				"@hooks": path.resolve(__dirname, "src/hooks"),
+				"@emails": path.resolve(__dirname, "src/emails"),
+				"@stores": path.resolve(__dirname, "src/stores"),
+			},
+			resolveExtensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+		},
+	},
 	env: {
 		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
 		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
