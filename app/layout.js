db7c2fd44e9a34d9b0feba3d4fc1b7e8551f80c7
Byron Wade <bcw1995@gmail.com>
2024-07-24 00:54:00 -0700

    more

--- app/layout.js
+++ app/layout.js
@@ -40,6 +40,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 				<script type="application/ld+json" dangerouslySetInnerHTML={addGameJsonLd()} key="game-jsonld" />
 			</head>
 			<body className={cn("min-h-screen bg-white dark:bg-black font-sans antialiased", fontSans.variable)} suppressHydrationWarning={true}>
